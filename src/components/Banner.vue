3b1d8f5299c0aecdc07a96137d83f814dc1625d4
wilber <parionahuaraca@gmail.com>
2024-07-19 14:39:17 -0500

    cambiosd

---
+++ src/components/Banner.vue
@@ -0,0 +1,145 @@
+<template>
+<v-container class="banner banner-content"  id="Acercademi"> 
+
+  <v-layout row wrap>  
+    <v-flex xs12 sm12 md4 lg5 xl4 >  
+      <div class="banner-bio">
+        <h2>¡Hola! Soy Wilber.</h2>
+        <p>
+          Con más de 4 años de experiencia en desarrollo web y un sólido conocimiento técnico, tengo la capacidad de contribuir de manera significativa a cualquier equipo y proyecto en los que participe.
+
+
+          <br> <br>
+          Durante mi carrera, he trabajado en diversos proyectos que incluyen desde sitios web empresariales hasta tiendas en línea, enfocándome siempre en crear experiencias de usuario intuitivas y visualmente atractivas. Mi habilidad para adaptarme rápidamente a nuevas tecnologías me permite abordar desafíos diversos y mantenerme al día con las tendencias actuales en diseño y desarrollo web.      </p>
+       </div>
+       
+      <v-btn class="banner-btn large fill-bg  "  round color="secondary"   data-aos="fade-up" outline  data-aos-offset="200" data-aos-delay="800"  data-aos-once="true" href="https://drive.google.com/file/d/1y5VNNMa_riXkiL3pUxFGcYJcITtKeK3-/view" target="_blank"   >Descargar CV </v-btn>
+      <v-btn class="banner-btn  large banner-btn--whatsapp"   round  color="secondary"    outline data-aos="fade-up"  data-aos-offset="200" data-aos-delay="1200" data-aos-once="true" href="https://api.whatsapp.com/send?phone=51938343796&text=Hola, Wilber" target="_blank" > WhatsApp </v-btn>
+      <div class="banner-avatar d-inline-flex py-3 px-4" outline data-aos="fade-up"  data-aos-offset="200" data-aos-delay="1500"  >
+        <div  class="banner-avatar-image" >
+          <img  class=" " src="../assets/wilberpariona.jpg" alt="Github"> 
+              <v-btn class="mx-2 social-icon  " href="https://github.com/WilberCode" target="_blank" >
+                <v-icon size="18px"> {{'fab fa-github'}} </v-icon>
+             </v-btn> 
+        </div>
+        <div class="d-flex  align-center">
+           <div class="ml-4">
+             <p class="mb-2 font-weight-bold banner-avatar-number	">Teléfono: <a href="tel:+51926776516">926776516</a></p>  
+             <p  class="mb-0 " >Lima - Perú</p> 
+           </div>
+        </div>
+      </div>
+    </v-flex>
+    <v-flex xs12 sm12 md8 lg7 xl8>  
+      <div class=" banner-image text-sx-center relative text-sm-center text-md-center text-lg-right" >
+        <img :src="require('../assets/me2.svg')" data-aos="fade-up"  data-aos-offset="200"  data-aos-once="true" data-aos-delay="1800" >
+      </div>
+      </v-flex>
+  </v-layout> 
+</v-container>
+</template>
+<script>
+
+export default {
+    data(){
+      return{
+     
+      }
+  
+    } 
+}
+</script>
+<style lang="scss">
+ 
+$md: 959px; 
+.banner{
+  &-content{
+    position:relative;
+    z-index: 5;
+  }
+  &-bio{
+    h2{
+      margin-top:1em;
+      margin-bottom:.8em;
+    }
+    color:#fff;
+  }
+  
+  &-btn{
+    font-size:14px;
+    color:#000000;
+    text-transform:none;
+    padding:1.8rem 2em;
+    margin-left:0;  
+    border-radius: 14px;
+    font-size: 18px;    
+    &--whatsapp{
+      border-width: 2px !important; 
+    }
+    @media (max-width:600px) {
+      padding:1.8rem 1.5em; 
+      
+    }
+  }
+  & .fill-bg.v-btn.v-btn--outline{
+    background: var(--v-secondary-base) !important;
+    color:#202aab!important;
+     border-width: 2px !important;
+     border-color: var(--v-secondary-base) ;
+  }
+  &-avatar{
+
+    box-shadow: var(--shadow-lg);
+    border-radius: 14px;
+    overflow: hidden;
+    background: #fff; 
+    width: 100%;
+    max-width: 350px;
+    margin-top: 2em;
+    &-image{
+      flex: initial !important; 
+      position: relative;
+      .social-icon {
+          width: 38px !important;
+          height: 38px !important;
+          position: absolute;
+          bottom: -15px;
+          left: -8px;
+          background: #000;
+          display: inline-flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 100%;
+          min-width: auto;
+          border:2px solid #000; 
+           box-shadow: none !important; 
+           background-color: #fff !important;
+      }
+    }
+    &-number{
+      font-size: 15px;
+    }
+    img{
+      width: 80px; 
+      display: block;
+      border-radius: 50%;
+      background:var(--v-primary-lighten5);
+    }
+  }
+  &-image{
+    margin-top:2em ; 
+    img{
+      @media (max-width:$md){
+        width:70%; 
+        margin-top:-15px;
+      }
+    //  @media (max-width:var(--sm)){
+    //     width:20%; 
+    //   }
+    }
+   @media (min-width:360px) and  (max-width:645px) {
+      margin-top:7em;
+   }
+  }
+}
+</style>
---
+++ src/components/KnowledgeSecondary.vue
@@ -0,0 +1,95 @@
+<template>  
+<div data-aos="fade-up"  data-aos-offset="200"  data-aos-once="true" data-aos-id="knoledgeaos" data-aos-delay="100" > 
+        <div class="text-xs-center">
+          <v-container grid-list-sm>
+            <v-layout row wrap>
+           
+              
+               <v-flex xs12 sm6 md4 lg3 xl3>  
+                <v-progress-circular
+                :rotate="180"
+                :size="100"
+                :width="15"
+                :value="valueProgress"
+                color="pink"
+                >
+                {{ valueProgress }}%
+                </v-progress-circular>  
+                <p>Wordpress, CPT, ACF, Api Rest, Elementor, Themes, Plugins, WooCommerce & SEO</p>
+              </v-flex>   
+              
+              <v-flex xs12 sm6 md4 lg3 xl3>  
+                <v-progress-circular
+                :rotate="180"
+                :size="100"
+                :width="15"
+                :value="valueProgress-5"
+                color="blue lighten-1"
+                >
+                {{ valueProgress-5 }}%
+                </v-progress-circular>  
+                <p>PHP, Laravel, Livewire, Alpine & Filament </p>
+              </v-flex> 
+              <v-flex xs12 sm6 md4 lg3 xl3>  
+                <v-progress-circular
+                :rotate="180"
+                :size="100"
+                :width="15"
+                :value="valueProgress-10"
+                color="green "
+                >
+                {{ valueProgress-10 }}%
+                </v-progress-circular>  
+                <p>Shopify & PrestaShop</p>
+              </v-flex>
+              <v-flex xs12 sm6 md4 lg3 xl3>  
+                <v-progress-circular
+                :rotate="180"
+                :size="100"
+                :width="15"
+                :value="valueProgress-40"
+                color="orange "
+                >
+                {{ valueProgress-40 }}%
+                </v-progress-circular>  
+                <p>VPS & Docker</p>
+              </v-flex>
+            </v-layout>
+          </v-container>  
+
+    </div>
+    </div>
+</template>
+<script>  
+
+ export default {
+    data () {
+      return {
+        interval: {},
+        valueProgress: 0,  
+      }
+    }, 
+    mounted () { 
+      this.getValue()   
+      
+    },
+    methods:{
+       getValue(){
+           document.addEventListener('aos:in:knoledgeaos', () => { 
+            this.valueProgress = 100
+          });  
+       } 
+    } 
+     
+  }  
+   
+</script>
+<style lang="scss">
+    .v-progress-circular{ 
+        margin: 1rem
+    }
+  .v-progress-circular__overlay{
+    transition: all 1s ease-in-out;
+  }
+
+</style>
